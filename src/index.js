bc58342abe8ccd3b4649347a92f430b15ee75f6d
lockys <locky4567@gmail.com>
2015-10-30 22:39:51 +0800

    add all json

--- src/index.js
+++ src/index.js
@@ -11,10 +11,9 @@ var getCateList = function(e, cate) {
   cate = cate || 'null';
   var list;
   d = [];
-
+  $awesome.addClass('content-hidden');
   $.getJSON('https://raw.githubusercontent.com/lockys/awesome-search/gh-pages/data/' + cate + '.json', function(data) {
     list = data;
-    $awesome.html('');
 
     if (cate !== 'awesome') {
       var repoURL = $(e.target).data('url');
@@ -23,6 +22,7 @@ var getCateList = function(e, cate) {
       $awesome.append('<a href="/awesome-search/"><- Back to Awesome</a><br/><a href="' + repoURL + '" target="_blank">-> Original Repo</a>');
     }
 
+    $awesome.html('');
     $searchResult.html('');
 
     Object.keys(list).forEach(function(e) {
@@ -31,7 +31,7 @@ var getCateList = function(e, cate) {
       $awesome.append(title);
       list[e].forEach(function(e) {
         var id = e.name.replace(/\W/g, '').toLowerCase();
-        var href = id === 'nodejs' || id === 'awesome' ? '' : ' href="' + e.url + '" ';
+        var href = '';
         var description = e.description ? ' - ' + e.description : '';
         var link = '<a class="mui-btn mui-btn--small mui-btn--primary ' + id + '"' + href + 'target="_blank" data-url="' + e.url + '" data-name="' + e.name + '"><span class="mui--text-white" data-url="' + e.url + '" data-name="' + e.name + '">' +  e.name + '</span><span class="mui--text-black-54" data-url="' + e.url + '" data-name="' + e.name + '">' + description + '</span></a>';
         $awesome.append(link);
@@ -40,17 +40,20 @@ var getCateList = function(e, cate) {
 
     f = new Fuse(d, options);
     $.getJSON('https://raw.githubusercontent.com/lockys/awesome-search/gh-pages/data/nameMap.json', function(data) {
-      Object.keys(data).forEach(function(ele) {
-        var cate = data[ele];
-        $('.' + cate).on('click', function(e) {
-          if (onClick) {
-            return false;
-          }
+      var idArr = Object.keys(data);
+      for (var i = 0, len = idArr.length; i < len; ++i) {
+        var cate = data[idArr[i]];
+        (function(cate) {
+          $('.' + cate).on('click', function(e) {
+            if (onClick) {
+              return false;
+            }
 
-          onClick = true;
-          getCateList(e, cate);
-        });
-      });
+            onClick = true;
+            getCateList(e, cate);
+          });
+        })(cate);
+      }
 
       $awesome.removeClass('content-hidden');
     });
@@ -77,7 +80,7 @@ $('.awesome-input').on('input', function(e) {
   for (var i = 0, len = result.length; i < len; ++i) {
     if (result[i]) {
       var id = result[i].name.replace(/\W/g, '').toLowerCase();
-      var href = id === 'nodejs' || id === 'awesome' ? '' : ' href="' + e.url + '" ';
+      var href = '';
       description = result[i].description ? ' - ' + result[i].description + '</br>' : '<br/>';
       $searchResult.append('<a class="' + id + '"' + href + 'target="_blank" data-url="' + result[i].url + '" data-name="' + result[i].name + '">' +  result[i].name + '</a>' + description);
       (function(id) {

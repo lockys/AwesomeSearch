98eb81d5b278b46a60972727416d7b8e92e41f49
lockys <locky4567@gmail.com>
2015-10-30 10:45:55 +0800

    minify and uglify js

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var gulpLoadPlugins = require('gulp-load-plugins');
+var path = {
+    src: './build/',
+    dist: './build/',
+  };
+
+var plugins = gulpLoadPlugins({
+  pattern: ['gulp-*'],
+  replaceString: /\bgulp[\-.]/,
+});
+
+gulp.task('js', function() {
+  gulp.watch(path.src + '*.js', ['js']);
+  return gulp.src(path.src + '*.js')
+    .pipe(plugins.concat('index.js'))
+    .pipe(plugins.rename({ suffix: '.min' }))
+    .pipe(plugins.uglify())
+    .pipe(gulp.dest(path.dist))
+    .pipe(plugins.notify({ message: 'Scripts Task Finished!' }));
+});
+
+gulp.task('default', ['js']);
